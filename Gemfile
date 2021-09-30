32dfee29a363c02f022488c09555c8eccd9c52c3
Jakub Żelazny <zelazny.jakub.jerzy@gmail.com>
2021-09-30 22:03:13 +0200

    Tailwind config

---
+++ Gemfile
@@ -0,0 +1,2 @@
+gem "jekyll", "~> 4.2.1"
+gem "webrick", "~> 1.7"
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.html",
+    "./*.html",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
