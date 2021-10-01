fe98deb8440c2ca2d49b433f220e9656308e8be3
Jakub Żelazny <zelazny.jakub.jerzy@gmail.com>
2021-10-01 14:15:03 +0200

    Add Tailwind css file manually

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 // postcss.config.js
 
-const cssnano = require("cssnano")({ preset: "default" })
-
 module.exports = {
   plugins: [
     require("autoprefixer"),
     require("tailwindcss")("./tailwind.config.js"),
-    ...(process.env.NODE_ENV === "production" ? [cssnano] : []),
+    require("cssnano")({ preset: "default" }),
   ],
 }
